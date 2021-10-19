317f21d877882dfc3d218663d5b332e4d29a3b0b
Subin Cho <subinee1633@gmail.com>
2021-10-19 11:43:03 -0400

    first commit

---
+++ frontend/todo_redux.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", function () {
+    const root = document.getElementById('root');
+    
+});
