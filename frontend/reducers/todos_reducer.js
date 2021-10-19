e11fa5bccc7e4e0065a91547b78aa770dda1b893
ggmu1986 <gt2186@gmail.com>
2021-10-19 12:22:02 -0400

    created all folders in fromntend and made reducer and store

---
+++ frontend/reducers/todos_reducer.js
@@ -0,0 +1,13 @@
+// import { RECEIVE_TODOS } from "../actions/todo_actions";
+
+
+export const todosReducer = (state = {}, action) => {
+    
+    switch (action.type) {
+        // case RECEIVE_TODOS: 
+        //     return {} 
+    default: 
+        return state;
+    }
+
+}
--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store'
 
 document.addEventListener("DOMContentLoaded", function () {
     const root = document.getElementById('root');
-    
+    const store = configureStore();
+    window.store = store;
 });
